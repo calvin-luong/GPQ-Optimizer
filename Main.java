9ab81c4ff242790f2372a1721361c031f03277c1
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2021-02-18 23:48:51 -0800

    Sorting and inserting finished.
    Kannas are added into own party
    Sharp eyes are added to own list
    Rest are added and sorted

--- Main.java
+++ Main.java
@@ -9,12 +9,18 @@ public class Main {
 	private int counter = 0;
 	
 	private static ArrayList<Party> result = new ArrayList<Party>();
+	private static ArrayList<Character> characterList = new ArrayList<Character>();
+	private static ArrayList<Character> SEList = new ArrayList<Character>();
 	
-	private static ArrayList<String> sharpEyes = new ArrayList<String>(Arrays.asList("Pathfinder", "Bowmaster", "Marksman", "Wind Archer", "Wild Hunter"));
+	private static ArrayList<String> SE = new ArrayList<String>(Arrays.asList("Pathfinder", "Bowmaster", "Marksman", "Wind Archer", "Wild Hunter"));
+	
+	// Assuming that 100k stat is impossible to reach in this current state of Maplestory
+	private static double lowestStat = 100;
+	// Lowest possible stat in Maplestory
+	private static double highestStat = 0;
 
 	public static void main(String[] args)
 	{
-		
 		try {
 			File txtFile = new File("Guild.txt");
 			Scanner reader = new Scanner(txtFile);
@@ -22,14 +28,91 @@ public class Main {
 				String currentLine = reader.nextLine();
 				
 				String[] character = currentLine.split(" ");
+				// Creating character object
 				Character c = new Character(character[0], character[1], Integer.parseInt(character[2]));
-				System.out.println(c);
+				
+				// Check and updating lowest stat in the text file
+				if (c.getStat() < lowestStat)
+				{
+					lowestStat = c.getStat();
+				}
+				
+				// Check adn updating highest stat in the text file
+				if (c.getStat() > highestStat)
+				{
+					highestStat = c.getStat();
+				}
+				
+				// Creates party with a kanna
+				if (c.getJob().equals("Kanna"))
+				{
+					Party p = new Party(null, null, null, null, null, c);
+					result.add(p);
+				}
+				// Add to sharpeyes list
+				else if (SE.contains(c.getJob()))
+				{
+					SEList.add(c);
+				}
+				// Adds to character list
+				else
+				{
+					characterList.add(c);
+				}
 			}
 			reader.close();
+			
+			// Testing if objects and lists are being created properly
+			System.out.println("Current parties: " + result);
+			System.out.println("Character pool: " + characterList);
+			System.out.println("Sharp eyes list" + SEList);
+			System.out.println("lowest stat:" + lowestStat);
+			System.out.println("highest stat:" + highestStat);
+			
+			sortList(characterList, 0, characterList.size() - 1);
+			System.out.println("Sorted: " + characterList);
 			
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
 	}
+	
+	public static void sortList(ArrayList<Character> list, int low, int high)
+	{
+		if (low < high)
+		{
+			int pivIndex = partition(list, low, high);
+			
+			sortList(list, low, pivIndex - 1);
+			sortList(list, pivIndex + 1, high);
+		}
+	}
+	
+	public static int partition(ArrayList<Character> list, int low, int high)
+	{
+		double piv = characterList.get(high).getStat();
+		int x = low - 1;
+		
+		for (int i = low; i < high; i++)
+		{
+			// When pointer is smaller than pivot pointer
+			if (characterList.get(i).getStat() < piv)
+			{
+				x++;
+				
+				// Swap
+				Character temp = characterList.get(x);
+				characterList.set(x, characterList.get(i));
+				characterList.set(i, temp);
+			}
+		}
+		
+		Character temp = characterList.get(x + 1);
+		characterList.set(x + 1, characterList.get(high));
+		characterList.set(high, temp);
+		
+		return x + 1;
+	}
+	
 }
